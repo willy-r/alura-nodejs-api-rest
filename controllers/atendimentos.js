815a41b8b644e2e11a25662cce8b8eddb1136d27
William Rodrigues <williamrodrigues2442@gmail.com>
2021-10-30 15:45:12 -0300

    Creates POST route to atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,8 +4,7 @@ module.exports = (app) => {
   // req = requisição, o que tá vindo
   // res = resposta, o que tá indo
   
-  /**
-   *  Rota para buscar todos os atendimentos.
-   */
   app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos, e está realizando um GET!'));
+
+  app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos, e está realizando um POST!'));
 }
