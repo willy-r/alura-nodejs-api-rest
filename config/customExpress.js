eb65032f070935759b3983bbfe758b0ba88a8010
William Rodrigues <williamrodrigues2442@gmail.com>
2021-10-30 16:22:03 -0300

    Adds configs to parse JSON and urlencoded

--- config/customExpress.js
+++ config/customExpress.js
@@ -6,6 +6,9 @@ const consign = require('consign');
 module.exports = () => {
   const app = express();
 
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
   // O consign faz a "ligação" das rotas para que o app consiga acessá-las de outra pasta.
   consign().include('controllers').into(app);
 
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,8 +3,10 @@
 module.exports = (app) => {
   // req = requisição, o que tá vindo
   // res = resposta, o que tá indo
-  
   app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos, e está realizando um GET!'));
 
-  app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos, e está realizando um POST!'));
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body);
+    res.send('Você está na rota de atendimentos, e está realizando um POST!')
+  });
 }
