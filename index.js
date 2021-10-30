93edea2ffe257f314bebf6c228835d4a6534a762
William Rodrigues <williamrodrigues2442@gmail.com>
2021-10-30 16:53:38 -0300

    Creates connection and connects to MySQL

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
 
-const app = customExpress();
-const PORT = 3000;
+// Conecta com o banco de dados.
+// Faz sentido subir o servidor mesmo se o banco falhar em conectar?
+conexao.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // Conexão com o banco deu certo, ativa o servidor.
+    console.log('Conectado no banco de dados com sucesso!');
+    
+    const app = customExpress();
+    const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+  }
+});
---
+++ infraestrutura/conexao.js
@@ -0,0 +1,12 @@
+const mysql2 = require('mysql2');
+
+// Configura conexão do banco de dados.
+const conexao = mysql2.createConnection({
+  host: 'localhost',
+  port: 2442,
+  user: 'root',
+  password: '1234',
+  database: 'agenda_petshop',
+});
+
+module.exports = conexao;
