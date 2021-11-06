c96311bcfc7ea7c68aac222ab4b17dad36ffb01f
William Rodrigues <williamrodrigues2442@gmail.com>
2021-11-06 13:45:54 -0300

    Updates env variable from SERVER_PORT to PORT

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ conexao.connect((err) => {
     Tabelas.init(conexao);
     
     const app = customExpress();
-    const PORT = process.env.SERVER_PORT || 3000;
+    const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
   }
